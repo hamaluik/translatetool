dc251fb128c89f8b7038898cd1a2d78bc5bc56d0
Kenton Hamaluik <kenton@hamaluik.ca>
2020-04-03 09:01:13 -0600

    started more comprehensive translations

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use fluent::{FluentBundle, FluentResource};
 use std::collections::HashMap;
 use std::env;
 use std::error::Error;
@@ -19,7 +18,7 @@ fn get_token_and_project_id(
     let credentials_file = matches.value_of("credentials").unwrap();
     let credentials_path = PathBuf::from(credentials_file);
     if !credentials_path.exists() {
-        eprintln!("you must provide a credentials files!");
+        log::error!("you must provide a credentials files!");
         return Err(Box::from(errors::Errors::MissingCredentialsFile));
     }
 
@@ -30,70 +29,40 @@ fn get_token_and_project_id(
     Ok((token, project_id))
 }
 
-fn load_strings<P: AsRef<Path>, S: ToString>(source: P, locale: S) -> Result<HashMap<String, (String, Vec<String>)>, Box<dyn Error>> {
-    let source_contents = fs::read_to_string(source)?;
-    let resource =
-        FluentResource::try_new(source_contents).map_err(|_| errors::Errors::CantParseResource)?;
-    let mut bundle = FluentBundle::new(&[locale]);
-    bundle
-        .add_resource(&resource)
-        .map_err(|_| errors::Errors::CantParseResource)?;
+fn continue_parsing<'ast, P: AsRef<Path>>(path: P, r: Result<fluent_syntax::ast::Resource<'ast>, (fluent_syntax::ast::Resource<'ast>, Vec<fluent_syntax::parser::ParserError>)>) -> fluent_syntax::ast::Resource<'ast> {
+    match r {
+        Ok(r) => r,
+        Err((r, errs)) => {
+            for err in errs {
+                log::warn!("parse error in {}: {:?}", path.as_ref().display(), err);
+            }
+            r
+        }
+    }
+}
 
-    let mut strings: HashMap<String, (String, Vec<String>)> = HashMap::with_capacity(bundle.entries.len());
-    for (term, entry) in bundle.entries.iter() {
-        // extract all the variables from this message
-        let mut variables: Vec<String> = Vec::new();
-        if let fluent_bundle::entry::Entry::Message(msg) = entry {
-            if let Some(pattern) = &msg.value {
-                for element in pattern.elements.iter() {
-                    if let fluent_syntax::ast::PatternElement::Placeable(expr) = element {
-                        if let fluent_syntax::ast::Expression::InlineExpression(iexpr) = expr {
-                            if let fluent_syntax::ast::InlineExpression::VariableReference {
-                                id,
-                            } = iexpr
-                            {
-                                variables.push(id.name.to_owned());
-                            } else {
-                                variables.push(format!("unparsable-var-{}", variables.len()));
-                            }
-                        } else {
-                            variables.push(format!("unparsable-var-{}", variables.len()));
-                        }
-                    }
+fn find_message<'ast>(resource: &'ast fluent_syntax::ast::Resource<'ast>, id: &str) -> Option<&'ast fluent_syntax::ast::Message<'ast>> {
+    for entry in resource.body.iter() {
+        if let fluent_syntax::ast::ResourceEntry::Entry(entry) = entry {
+            if let fluent_syntax::ast::Entry::Message(message) = &entry {
+                if message.id.name == id {
+                    return Some(message);
                 }
             }
         }
-
-        // get the text of the string
-        let result = bundle.format(term, None);
-        if result.is_none() {
-            return Err(Box::from("Only messages can be parsed at this moment, not terms or functions! Sorry!"));
-        }
-        let (text, errs) = result.unwrap();
-        for err in errs {
-            eprintln!("Error formatting string: {}", err);
-        }
-
-        // save it
-        strings.insert(term.to_owned(), (text, variables));
     }
-
-    Ok(strings)
-}
-
-fn emplace_vars(mut text: String, vars: Vec<String>) -> String {
-    for var in vars.into_iter() {
-        text = text.replacen("___", &format!("{{ ${} }}", var), 1);
-    }
-    text
+    None
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    simplelog::CombinedLogger::init(vec![
+        simplelog::TermLogger::new(simplelog::LevelFilter::Debug, simplelog::Config::default(), simplelog::TerminalMode::Mixed).expect("can init termlogger")
+    ]).expect("can initiate logging");
     let matches = cli::build_cli().get_matches();
 
     if let Some(_submatches) = matches.subcommand_matches("languages") {
         let (token, project_id) = get_token_and_project_id(&matches).map_err(|e| {
-            eprintln!(
+            log::error!(
                 "failed to get token and project id from credentials file: {:?}",
                 e
             );
@@ -101,7 +70,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         })?;
         let translator = translate::Translator::new(&token, &project_id, "en");
         let available_languages = translator.available_languages().map_err(|e| {
-            eprintln!("failed to list available languages from translator!");
+            log::error!("failed to list available languages from translator!");
             e
         })?;
 
@@ -133,7 +102,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let (token, project_id) = get_token_and_project_id(&matches)?;
     let from_file = matches.value_of("from").unwrap();
-    let diff_path: Option<&str> = matches.value_of("diff");
+    let diff_path: Option<PathBuf> = matches.value_of("diff").map(PathBuf::from);
     let locale = matches
         .value_of("locale")
         .ok_or(errors::Errors::MissingLanguage)?;
@@ -148,7 +117,84 @@ fn main() -> Result<(), Box<dyn Error>> {
         .find(|lang| lang.language_code == locale)
         .ok_or(errors::Errors::InvalidLanguage)?;
 
-    let mut english_source = load_strings(from_file, "en")?;
+    let source = std::fs::read_to_string(from_file)?;
+    let source_outdated = if let Some(diff_path) = &diff_path {
+        if diff_path.exists() {
+            std::fs::read_to_string(diff_path)?
+        }
+        else {
+            String::new()
+        }
+    }
+    else {
+        String::new()
+    };
+    let target_existing = if out_path.exists() {
+        std::fs::read_to_string(&out_path)?
+    }
+    else {
+        String::new()
+    };
+
+    let source = continue_parsing(&from_file, fluent_syntax::parser::parse(&source));
+    let source_outdated = continue_parsing(&diff_path.unwrap_or_default(), fluent_syntax::parser::parse(&source_outdated));
+    let target_existing = continue_parsing(&out_path, fluent_syntax::parser::parse(&target_existing));
+
+    let mut translations: HashMap<&str, Option<String>> = HashMap::new();
+
+    for entry in source.body.iter() {
+        if let fluent_syntax::ast::ResourceEntry::Entry(entry) = entry {
+            if let fluent_syntax::ast::Entry::Message(message) = &entry {
+                // check if we need to translate based on diffs
+                let needs_translation: bool = if let Some(outdated) = find_message(&source_outdated, message.id.name) {
+                    message.value != outdated.value
+                }
+                else {
+                    true
+                };
+
+                // disable translation if we have a hand-translated one
+                let needs_translation = if let Some(existing) = find_message(&target_existing, message.id.name) {
+                    if let Some(comment) = &existing.comment {
+                        if let fluent_syntax::ast::Comment::Comment{ content } = comment {
+                            !content.iter().any(|c| c.contains("tt-hand-translated"))
+                        } else { needs_translation }
+                    }
+                    else { needs_translation }
+                }
+                else {
+                    needs_translation
+                };
+
+                if needs_translation {
+                    if let Some(pattern) = &message.value {
+                        // prepare the pattern for translating by stripping placeables
+                        let source_formatted: String = pattern.elements.iter().map(|pe| match pe {
+                            fluent_syntax::ast::PatternElement::TextElement(s) => s,
+                            fluent_syntax::ast::PatternElement::Placeable(_) => "___",
+                        }).collect();
+
+                        translations.insert(message.id.name, Some(match translator.translate(&source_formatted) {
+                            Ok(t) => t,
+                            Err(e) => {
+                                log::warn!("failed to translate term `{}`: {:?}", message.id.name, e);
+                                source_formatted
+                            }
+                        }));
+                    }
+                    else {
+                        translations.insert(message.id.name, None);
+                    }
+                }
+            }
+        }
+    }
+
+    for t in translations {
+        log::debug!("translated `{}` => `{:?}`", t.0, t.1);
+    }
+
+    /*let mut english_source = load_strings(from_file, "en")?;
     let old_english_source: HashMap<String, String> =
         if out_path.exists() {
             if let Some(diff_path) = diff_path {
@@ -253,7 +299,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         let message = mung::decode_entities(translation);
         file.write_fmt(format_args!("{} = {}\n\n", term, message))?;
     }
-    println!("translations saved to file: {}", out_path.display());
+    println!("translations saved to file: {}", out_path.display());*/
 
     Ok(())
 }
